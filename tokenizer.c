7c6270c0387e6dbc4531530c172509905f7d9e43
Edwin <etrejo9@miners.utep.edu>
2022-02-02 20:57:20 -0700

    implemented delim_character and non_delim_character functions

--- tokenizer.c
+++ tokenizer.c
@@ -2,10 +2,31 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+bool delim_character(char c, char delim)
+{
+	if(c == delim) {
+		return true;
+	 }
+	return false;
+}
 
+bool non_delim_character(char c, char delim)
+{
+	if(c != delim) {
+		return true;
+	 }
+	return false;
+}
+
+
+        
 int main()
 {
-	printf("Welcome! input a string");	
+
+	char delim[50];
+	printf("Welcome! Select a delimeter\n");
+	fgets(delim,50,stdin);
+	printf("Selected delimeter: %s", delim);
 
 	return 0;
 }
